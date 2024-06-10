25010d62ae1c99abff5b6206e1e3d1faae916cec
Łukasz Sarnacki <lukasz.sarnacki@gmail.com>
2024-06-10 11:23:11 +0000

    I have no idea

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -5,6 +5,67 @@
 
 #pragma once
 
+// Left-hand home row mods
+#define HOME_A U_MT(LGUI, A)
+#define HOME_S U_MT(LALT, S)
+#define HOME_D U_MT(LCTRL, D)
+#define HOME_F U_MT(LSHIFT, F)
+
+// Right-hand home row mods
+#define HOME_J U_MT(RSHIFT, J)
+#define HOME_K U_MT(RCTRL, K)
+#define HOME_L U_MT(LALT, L)
+#define HOME_SEMI U_MT(LGUI, SEMI)
+
+// #define LAYER_BASE_NIX \
+// &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+// HOME_A,     HOME_S,     HOME_D,    HOME_F,    &kp G,             &kp H,             HOME_J,    HOME_K,    HOME_L,     HOME_SEMI,   \
+// U_LT(U_BUTTON, Z), &kp X,     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,   U_LT(U_BUTTON, SLASH),\
+// U_NP,              U_NP,              U_LT(U_FUN, ESC),U_LT(U_SYM2, SPACE),U_LT(U_NAV, TAB),U_LT(U_NUM, RET),  U_LT(U_SYM, BSPC), U_LT(U_MOUSE, DEL),  U_NP,              U_NP
+
+#define LAYER_BASE_NIX \
+&kp Q,             U_MT(RALT, W),             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             U_MT(RALT, O),             &kp P,             \
+HOME_A,     HOME_S,     HOME_D,    HOME_F,    &kp G,             &kp H,             HOME_J,    HOME_K,    HOME_L,     HOME_SEMI,   \
+U_LT(U_BUTTON, Z), &kp X,     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_FUN, ESC),  U_LT(U_SYM2, BSPC), U_LT(U_NAV, TAB),  U_LT(U_NUM, RET),U_LT(U_SYM, SPACE),U_LT(U_MOUSE, DEL),U_NP,              U_NP
+
+#define LAYER_MOUSE_NIX \
+U_WH_U,            U_WH_L,            U_MS_U,            U_WH_R,            U_NU,              U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &kp RALT,       U_BOOT,            \
+U_WH_D,            U_MS_L,            U_MS_D,            U_MS_R,            U_NU,              U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_NA,              &u_to_U_MOUSE,     &u_to_U_SYM,       &u_to_U_TAP,          U_NA,              \
+U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define LAYER_NAV_NIX \
+U_BOOT,            &kp RALT,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
+U_NA,              &u_to_U_TAP,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           &kp INS,           \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_LT(U_MEDIA, RET),           &kp SPACE,          &kp DEL,           U_NP,              U_NP
+
+#define LAYER_SYM2_NIX \
+U_BOOT,            &kp RALT,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_NA,     &kp LBRC,          &kp RBRC,    &kp PLUS,   &kp PIPE,          \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,           U_NA,   &kp LBKT,          &kp RBKT,          &kp DQT,         &kp SQT,        \
+U_NA,              &u_to_U_TAP,          &u_to_U_MOUSE,     &u_to_U_SYM,       U_NA,              &kp LT,          &kp LPAR,    &kp RPAR,      &kp GT,          &kp BSLH,         \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,         U_NA,          U_NA,          U_NP,              U_NP
+
+#define LAYER_SYM_NIX \
+&kp GRAVE,          &kp AMPS,          &kp ASTRK,         &kp PRCNT,          U_NA,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,    &kp RALT,       U_BOOT,            \
+&kp EQUAL,         &kp CARET,          &kp MINUS,         &kp UNDER,         &kp DLLR,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          U_NA,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &u_to_U_TAP,          U_NA,              \
+U_NP,              U_NP,              U_NA,          U_NA,          U_NA,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define LAYER_FUN_NIX \
+U_BOOT,            &kp RALT,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp PSCRN,         &kp F7,            &kp F8,            &kp F9,            &kp F12,           \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp SLCK,          &kp F4,            &kp F5,            &kp F6,            &kp F11,           \
+U_NA,              &u_to_U_TAP,          &u_to_U_MEDIA,     &u_to_U_FUN,       U_NA,              &kp PAUSE_BREAK,   &kp F1,            &kp F2,            &kp F3,            &kp F10,           \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp TAB,           &kp SPACE,         &kp K_APP,         U_NP,              U_NP
+
+#define LAYER_MEDIA_NIX \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp C_STOP,         &kp C_PP,         &kp C_MUTE,         U_NA,         U_RGB_TOG,         \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG,          \
+U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       &u_out_tog,        \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,      U_NA,      U_NA,        U_NP,              U_NP
+// ----------------------------------------------------------------------------
+
 
 #define MIRYOKU_ALTERNATIVES_BASE_AZERTY_FLIP \
 &kp A,             &kp Z,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
